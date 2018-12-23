f5fa40a34d41b7f33a1029da15d07aff3de91c39
Emmanuel Semutenga <emzsema@gmail.com>
2018-12-23 15:27:21 +0300

    finished working on forms

---
+++ me.py
@@ -0,0 +1,12 @@
+name = ['fdsdd','ssdsd','sdsdfe']
+
+schools = {
+    'id':3443,
+    'teacher':'semujju',
+    'location':'mutundwe'
+}
+for i in range(len(name)):
+    print(name[i])
+
+for key, value in schools.items():
+    print(f'{key} {value}')
