ae78af2da1349fbb9018f8d0043fc41465b10b05
Emmanuel Semutenga <emzsema@gmail.com>
2018-12-24 10:11:44 +0300

    Added user authentication

---
+++ kblog/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'glkvfss;lksvsvsk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#'postgresql://postgres:getyours@localhost/flaskblog'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from kblog import routes
